9fe3e7bfcb1ad05f1f1043d547cf12c72c9c391c
Fathima-Zahra <67513163+Fathima-Zahra@users.noreply.github.com>
2020-07-19 17:28:32 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+function copy() {
+  alert("DOnT, Try TO CoPy , I am WaTChIng u , type It on ur Own");
+}
+
+function summer() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "100%";
+  alert("Woohoo , Its summer :) !");
+}
+
+function winter() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "100%";
+  alert("wooho. its snowtime :) ");
+}
+
+function spring() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "100%";
+  alert("lets move into a flowery time ");
+}
+
+function autumn() {
+  document.body.style.backgroundImage =
+    "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "100%";
+  alert("Time 4 halloween , ooooooh spooooky !!");
+}
